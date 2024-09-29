8f95c7249bdfbf48783aac3fdfc23a35f0bf0cc5
Callixte de Vulpillières <callixte@vulpillieres.fr>
2024-09-29 23:24:01 +0200

    Τ

--- AliceVsBob.py
+++ AliceVsBob.py
@@ -58,7 +58,7 @@ async def ajout_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def ban_on_word(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Create a regex to match "rust" and any variant with strange characters
     # like "rüst" or "rùst" or "Rսst"
-    rust_regex = re.compile(r"(r|г)(u|ս)st", re.IGNORECASE)
+    rust_regex = re.compile(r"(r|г)(u|ս)s(Τ|t)", re.IGNORECASE)
     # Check if the message contains the word "rust"
     if rust_regex.search(
         strip_accents(update.effective_message.text).translate(
