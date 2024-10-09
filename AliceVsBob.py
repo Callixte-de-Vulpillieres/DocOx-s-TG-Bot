295a02c7258ff1262c60011343b95ee39161d441
Callixte de Vulpillières <callixte@vulpillieres.fr>
2024-10-09 20:56:16 +0200

    lower

--- AliceVsBob.py
+++ AliceVsBob.py
@@ -84,14 +84,14 @@ def find_rust(text):
     ]
     st = ["ﬆ"]
     s = ["s", "ѕ", "տ", "ꮥ", "ꮪ", "𐑈", "ꞩ", "ꟊ", "🅂", "🅢", "🆂", "🇸", "n", "丂", "ƽ"]
-    t = ["ꭲ", "𑣜", "🝨", "τ", "t", "🇹", "🅃", "🅣", "🆃", "ҭ", "ꓤ", "ｲ", "ƚ", "l"]
+    t = ["ꭲ", "𑣜", "🝨", "τ", "t", "🇹", "🅃", "🅣", "🆃", "ҭ", "ꓤ", "ｲ", "ƚ", "l", "𝓣"]
     for i in range(len(text) - 3):
-        if text[i] in r or unidecode(text[i]) in r:
-            if text[i + 1] in u or unidecode(text[i + 1]) in u:
-                if text[i + 2] in s or unidecode(text[i + 2]) in s:
-                    if text[i + 3] in t or unidecode(text[i + 3]) in t:
+        if text[i] in r or unidecode(text[i]).lower() in r:
+            if text[i + 1] in u or unidecode(text[i + 1]).lower() in u:
+                if text[i + 2] in s or unidecode(text[i + 2]).lower() in s:
+                    if text[i + 3] in t or unidecode(text[i + 3]).lower() in t:
                         return True
-                elif text[i + 2] in st or unidecode(text[i + 2]) in st:
+                elif text[i + 2] in st or unidecode(text[i + 2]).lower() in st:
                     return True
     return False
 
